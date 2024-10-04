561fe00a135a6d87dcf8be8fc08d4dadf3c570fc
balpa <berkealtiparmak@outlook.com>
2024-10-04 09:47:22 +0300

    movie details page adding to favorites func done

--- src/components/Content/MovieDetailsContent.vue
+++ src/components/Content/MovieDetailsContent.vue
@@ -1,7 +1,9 @@
 <script setup>
     import { ref, onMounted, onBeforeUnmount, watch } from 'vue';
     import { getMovieById } from '../../functions/fetchData.js'
+    import { usePosterStore } from '../../stores/posterStore.js';
 
+    const posterStore = usePosterStore();
     const imagePrefix = 'https://image.tmdb.org/t/p/w220_and_h330_face';
 
     const canvasElement = ref();
@@ -11,12 +13,21 @@
     const posterURL = ref('');
     const voteAvg = ref('');
     const votePer = ref('');
+    const isCurrentPosterFavorited = ref(false);
 
     onMounted(() => {
         context.value = canvasElement.value?.getContext('2d');
 
         fetchData()
+
+        setTimeout(() => {
+            checkIsFavorited()
+        }, 1500);
     });
+
+    watch(() => isCurrentPosterFavorited.value, () => {
+        isCurrentPosterFavorited.value = checkIsFavorited();
+    }, { immediate: true });
 
     function render() {
         const ctx = context.value;
@@ -52,6 +63,24 @@
             loading.value = false;
         }
     }
+
+    function addToFavorites() {
+        if (!checkIsFavorited()) {
+            posterStore.addItems(movie?.value?.id);
+        } else {
+            posterStore.removeItems(movie?.value?.id);
+        }
+
+        isCurrentPosterFavorited.value = checkIsFavorited();
+    }
+
+    function checkIsFavorited() {
+        const favoritedPosters = posterStore.getItems;
+
+        isCurrentPosterFavorited.value = null;
+
+        return favoritedPosters.includes(movie?.value?.id);
+    }
 </script>
  
 <template>
@@ -64,19 +93,20 @@
                     <span class="text-[6px] mt-[-5px]">{{ votePer }}</span>
                     <canvas ref="canvasElement" class="percentage-canvas absolute rotate-[270deg]" width="38" height="38"></canvas>
                 </div>
+                <div @click="addToFavorites"  :class="{ 'favorited': isCurrentPosterFavorited }" class="poster-star text-[24px] absolute top-0 right-[5px] text-white cursor-pointer">&#9733;</div>
             </div>
             <div class="movie-info">
                 <h1>{{ movie.title }}</h1>
                 <p class="tagline">{{ movie.tagline }}</p>
                 <p class="overview">{{ movie.overview }}</p>
-                <ul class="details-list">
-                    <li><strong>Genres: {{ movie?.genres?.map(genre => genre.name).join(', ') }}</strong></li>
-                    <li><strong>Release Date: {{ movie.release_date }}</strong></li>
-                    <li><strong>Runtime: {{ movie.runtime }}</strong></li>
-                    <li><strong>Vote Average: {{ movie.vote_average }}</strong></li>
-                    <li><strong>Budget: {{ movie.budget }}</strong></li>
-                    <li><strong>Revenue: {{ movie.revenue }}</strong></li>
-                    <li><strong>Homepage:</strong> <a :href="movie.homepage" target="_blank">{{ movie.homepage }}</a></li>
+                <ul class="details-list font-[600]">
+                    <li><strong>Genres:</strong> {{ movie?.genres?.map(genre => genre.name).join(', ') }}</li>
+                    <li><strong>Release Date:</strong> {{ movie.release_date }}</li>
+                    <li><strong>Runtime:</strong> {{ movie.runtime }}</li>
+                    <li><strong>Vote Average:</strong> {{ movie.vote_average }}</li>
+                    <li><strong>Budget:</strong> {{ movie.budget }}</li>
+                    <li><strong>Revenue:</strong> {{ movie.revenue }}</li>
+                    <li><strong>Homepage:</strong> <a class="underline" :href="movie.homepage" target="_blank">{{ movie.homepage }}</a></li>
                 </ul>
             </div>
         </div>
